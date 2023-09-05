4727f303a778758e098325aa13c7591b0505a1b7
Mr Violets <140274934+MrViolets@users.noreply.github.com>
2023-09-05 10:09:36 +0100

    Add popup (#10)
    * Add popup
    
    * Add group now menu item
    
    * Run standard.js
    
    * Update extension name
    
    * Update icon
    
    * Update README.md
    
    * Add command
    
    * Update permissions

---
+++ src/preferences.js
@@ -0,0 +1,47 @@
+'use strict'
+
+/* global chrome */
+
+import * as ch from './chrome/promisify.js'
+
+export const defaults = {
+  enabled: {
+    title: chrome.i18n.getMessage('MENU_AUTO_GROUP'),
+    value: true,
+    type: 'checkbox'
+  },
+  auto_close_groups: {
+    title: chrome.i18n.getMessage('MENU_AUTO_CLOSE'),
+    value: true,
+    type: 'checkbox'
+  },
+  auto_collapse_groups: {
+    title: chrome.i18n.getMessage('MENU_AUTO_COLLAPSE'),
+    value: false,
+    type: 'checkbox'
+  }
+}
+
+export async function get () {
+  try {
+    const result = await ch.storageLocalGet({ preferences: defaults })
+    const userPreferences = result.preferences
+
+    for (const key in userPreferences) {
+      if (!(key in defaults)) {
+        delete userPreferences[key]
+      }
+    }
+
+    for (const defaultKey in defaults) {
+      if (!(defaultKey in userPreferences)) {
+        userPreferences[defaultKey] = defaults[defaultKey]
+      }
+    }
+
+    return userPreferences
+  } catch (error) {
+    console.error(error)
+    return defaults
+  }
+}
